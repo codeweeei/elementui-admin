1bc40445157b7e21889be47f1f61646d75e0c728
王威 <1749894019@qq.com>
2021-02-20 22:52:09 +0800

    完成路由跳转界面

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import CreateArticles from '../views/CreateArticles.vue'
+import ListArticles from "../views/ListArticles.vue"
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'ListArticles',
+    redirect: "/articles/index",
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/articles/create',
+    name: 'CreateArticles',
+    component: CreateArticles
+  },
+  {
+    path: '/articles/index',
+    name: 'ListArticles',
+    component: ListArticles
   }
 ]
 
---
+++ src/views/CreateArticles.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>新建文章</div>
+</template>
+
+<script>
+export default {
+  name:'CreateArticles'
+}
+</script>
+
+<style scoped>
+
+</style>
